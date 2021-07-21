8a0d4aab9deafc388786cbd02403f2117e6a9897
nikhilp132 <nikhilparadkar200@gmail.com>
2021-07-21 13:06:00 +0530

    2 files modified

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function nextSlide() {
 
   $('.slideContainer').animate({ left: '-200px' });
 }
+
+function prevSlide() {
+  var slideWidth = $('.slide1').width();
+
+  $('.slideContainer').animate({ left: '0px' });
+}
